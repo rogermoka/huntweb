2431c613eef2852bf9e10969a76f9e467c1baa94
rogermoka <roger.mokarzel@gmail.com>
2019-04-18 21:20:20 -0300

    listando produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './styles.css'
 
 export default class Main extends Component {
+    state = {
+        products: []
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
@@ -9,10 +14,24 @@ export default class Main extends Component {
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response);
+        this.setState({ products: response.data.docs })
+        //console.log(response.data.docs);
     }
 
     render() {
-        return <h1>Hello World</h1>;
+        const { products } = this.state;
+
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+            </div>
+            );
     }
 }
